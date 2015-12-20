a24296ba1a998a6ec6ec4f657499a8ad2923a1d7
YinYin Chiu <cychiuae@connect.ust.hk>
2015-12-21 00:13:32 +0800

    [Added request data]

--- BookSearch/BookList.js
+++ BookSearch/BookList.js
@@ -7,7 +7,10 @@ var {
 	StyleSheet,
 	Text,
 	View,
-	Component
+	Component,
+	ListView,
+	TouchableHighlight,
+	ActivityIndicatorIOS
 } = React;
 
 var FAKE_BOOK_DATA = [
@@ -37,21 +40,91 @@ var styles = StyleSheet.create({
 	},
 	author: {
 		color: '#656565'
-	}
+	},
+	separator: {
+		height: 1,
+		backgroundColor: '#dddddd'
+	},
+	listView: {
+    	backgroundColor: '#F5FCFF'
+  	},
+  	loading: {
+  		flex: 1,
+  		alignItems: 'center',
+  		justifyContent: 'center'
+  	}
 });
 
+var REQUEST_URL = 'https://www.googleapis.com/books/v1/volumes?q=subject:fiction';
+
 class BookList extends Component {
-	render() {
-		var book = FAKE_BOOK_DATA[0];
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true,
+			dataSource: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2
+			})
+		};
+	}
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData() {
+		fetch(REQUEST_URL)
+		.then((response) => response.json())
+		.then((responseData) => {
+			this.setState({
+				dataSource: this.state.dataSource.cloneWithRows(responseData.items),
+				isLoading: false
+			});
+		})
+		.done();
+	}
+
+	renderBook(book) {
 		return (
-			<View style={styles.container}>
-				<Image source={{uri: book.volumeInfo.imageLinks.thumbnail}}
-						style={styles.thumbnail} />
-				<View style={styles.rightContainer}>
-					<Text style={styles.title}>{book.volumeInfo.title}</Text>
-					<Text style={styles.author}>{book.volumeInfo.author}</Text>
+			<TouchableHighlight>
+				<View>
+					<View style={styles.container}>
+						<Image
+							source={{uri:book.volumeInfo.imageLinks.thumbnail}}
+							style={styles.thumbnail} />
+						<View style={styles.rightContainer}>
+                            <Text style={styles.title}>{book.volumeInfo.title}</Text>
+                            <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+                        </View>
+					</View>
+					<View style={styles.separator} />
 				</View>
+			</TouchableHighlight>
+		);
+	}
+
+	renderLoadingView() {
+		return(
+			<View style={styles.loading}>
+				<ActivityIndicatorIOS size='large' />
+				<Text>
+					Loading Books....
+				</Text>
 			</View>
+		);
+	}
+
+	render() {
+		if (this.state.isLoading) {
+			return this.renderLoadingView();
+		}
+
+		return (
+			<ListView 
+				dataSource={this.state.dataSource}
+				renderRow={this.renderBook.bind(this)}
+				style={styles.listView} />
 		);
 	}
 }
